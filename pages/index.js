b42e45a9a14af2e4288c36dd5c46fd3fbcc62edc
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2020-10-30 23:02:54 +0800

    file directory switchup!!

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
