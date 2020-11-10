5f73dc5faa1375ab92f46c3e8b1e44ed7719a2f1
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2020-11-10 12:42:57 +0800

    captcha, error, validation and sorts

--- components/feedback.js
+++ components/feedback.js
@@ -8,6 +8,7 @@ import utilStyles from '../styles/utils.module.css'
 import Sparkles from './sparkle'
 
 import firebase from '../lib/firebase'
+import ReCAPTCHA from "react-google-recaptcha";
 
 import useSWR from 'swr'
 const fetcher = async (...args) => {
@@ -19,13 +20,49 @@ export default function Feedback(props) {
     const [state, setState] = useState({
         liked: false,
         comment: "",
-        name: ""
+        name: "",
+        email: "",
+        error: false,
+        captcha: false
     })
     const toggleLike = () => setState(prevState => ({ ...prevState, liked: !prevState.liked }))
-    const commentHandler = (e) => setState(prevState => ({ ...prevState, comment: e.target.value }))
-    const nameHandler = (e) => setState(prevState => ({ ...prevState, name: e.target.value }))
+
+    const commentHandler = (e) => {
+        setState(prevState => ({ ...prevState, comment: e.target.value }));
+        if(state.error) toggleError();
+    }
+    const nameHandler = (e) => {
+        setState(prevState => ({ ...prevState, name: e.target.value }));
+        if(state.error) toggleError();
+    }
+    const emailHandler = (e) => {
+        setState(prevState => ({ ...prevState, email: e.target.value }));
+        if(state.error) toggleError();
+    }
+
+    const clearState = () => {
+        setState(prevState => ({ ...prevState, name: '', comment: '', email: '' }))
+        console.log("cleared")
+    }
+
+    const captchaHandler = (val) => {
+        setState(prevState => ({ ...prevState, captcha: true }));
+        console.log(val);
+    }
+    const captchaErrorHandler = (val) => {
+        setState(prevState => ({ ...prevState, captcha: false }));
+        console.log(val);
+    }
+
+    const toggleError = () => setState(prevState => ({ ...prevState, error: !prevState.error }))
+
+    const validateEmail = (email) => {
+        if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+            return true;
+        } else { return false; }
+    }
     
-    const { data } = useSWR(`/api/stats/${props.post}`, fetcher);
+    const { data } = useSWR(`/api/stats/${props.post}`, fetcher, { refreshInterval: 100 });
     // if(data) console.log(data)
 
     const updateDoc = (doc, data) => {
@@ -33,16 +70,24 @@ export default function Feedback(props) {
         return;
     }
 
-    const submitComment = (doc, name, text) => {
-        firebase.firestore().collection('blogs').doc(doc).update({ 
-            comments: firebase.firestore.FieldValue.arrayUnion(
-                {
-                    name: name,
-                    date: new Date().toString(),
-                    text: text
-                }
-            )
-        })
+    const submitComment = (doc, name, text, email) => {
+        //email pun
+        // make sure recaptcha??
+        if(name && text && email && validateEmail(email) && state.captcha) {
+            if(state.error) toggleError();
+
+            firebase.firestore().collection('blogs').doc(doc).update({ 
+                comments: firebase.firestore.FieldValue.arrayUnion(
+                    {
+                        name: name,
+                        date: new Date().toString(),
+                        text: text,
+                        email: email
+                    }
+                )
+            })
+            clearState();
+        } else{ toggleError() }
     } 
 
     return (
@@ -68,31 +113,63 @@ export default function Feedback(props) {
             <table className={styles.input} role="presentation">
                 <tbody>
                     <tr>
-                        <td rowSpan="2" className={styles.commentInput}>
+                        <td colSpan="2" style={{ width: "150%", height: "150%" }}>
                             <Textarea 
                                 id="text" 
-                                status="secondary" width="100%" 
+                                width="100%" 
+                                status={state.error ? "error" : "secondary"} 
                                 placeholder="Comments!" 
                                 onChange={commentHandler}
-                            />
-                        </td>
-                        <td>
-                            <Input 
-                                id="name" 
-                                className={styles.name} 
-                                width="100%" height="100%" status="secondary" 
-                                placeholder="Name" 
-                                onChange={nameHandler}
+                                value={state.comment}
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td>
-                            <Button size="auto" type="secondary" ghost 
-                                onClick={() => submitComment(props.post, state.name, state.comment)}
+                        <td rowSpan="3" style={{}}>
+                            <ReCAPTCHA 
+                                theme="dark"
+                                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+                                onChange={captchaHandler}
+                                onExpired={captchaErrorHandler}
+                                onErrored={captchaErrorHandler}
+                            />
+                        </td>
+                        <td style={{}}>
+                            <Input 
+                                id="name" 
+                                width="100%"
+                                status={state.error ? "error" : "secondary"} 
+                                placeholder="Name" 
+                                onChange={nameHandler}
+                                value={state.name}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style={{}}>    
+                            <Input 
+                                id="email" 
+                                width="100%"
+                                status={state.error ? "error" : "secondary"} 
+                                placeholder="Email" 
+                                onChange={emailHandler}
+                                value={state.email}
+                            />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style={{}}>
+                            <Button size="large" type="secondary" ghost 
+                                onClick={() => submitComment(
+                                    props.post, 
+                                    state.name, 
+                                    state.comment, 
+                                    state.email
+                                )}
                             >post!! ヾ(•ω•`)o</Button>
                         </td>
                     </tr>
+                    
                 </tbody>
             </table>
 
@@ -104,7 +181,7 @@ export default function Feedback(props) {
 
             <div>
                 {!data ? <div className={styles.comment}>loading...</div> 
-                : data.comments.reverse().map(comment => (
+                : data.comments.slice().reverse().map(comment => (
                     <div className={styles.comment} key={comment.date}>
                         <div>
                             <h4>{comment.name}</h4>
---
+++ pages/api/spotify.js
@@ -0,0 +1,77 @@
+//https://github.com/rishiosaur/w/blob/master/src/functions/fetch.ts
+//https://github.com/rishiosaur/w/blob/master/pages/api/spotify.ts
+const client_id = process.env.NEXT_PUBLIC_SPOTIFY_ID;
+const client_secret = process.env.NEXT_PUBLIC_SPOTIFY_SECRET;
+const refresh_token = process.env.NEXT_PUBLIC_SPOTIFY_REFRESH;
+
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
+
+const getAccessToken = async () => {
+	const response = await fetch(TOKEN_ENDPOINT, {
+		method: 'POST',
+		headers: {
+			Authorization: `Basic ${basic}`,
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: querystring.stringify({
+			grant_type: 'refresh_token',
+			refresh_token,
+		}),
+	})
+
+	return response.json()
+}
+
+export const getNowPlaying = async () => {
+	const { access_token } = await getAccessToken()
+
+	return fetch(NOW_PLAYING_ENDPOINT, {
+		headers: {
+			Authorization: `Bearer ${access_token}`,
+		},
+	})
+}
+
+export default (req, res) => {
+    // curl -X "GET" "https://api.spotify.com/v1/me/player/currently-playing?market=ES&additional_types=episode" 
+    //      -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQA-NlJAY5gItmmuNKv90pnc6gfJmvQFjxsoCAc5s5tKrmZWdfczUDff5dG_k3XHLWyGYGy2Cmw8tGuhaTmbsZIWXGHVLbNimMGys81XIgK51QzJZBxH5-gCAV2CPdG2yfurjcI-mtSb-bdicA81C01cJZUD77MiAEAS2H6hFS7rFb7T7vcDmwPFo4R3DSQoONb5xpjCUsI1GzIqQ9IxNEtmZ1T5ObJs_h8_A_br05QjsqDPhYIfBzPFaQ8wkRHVxkk_PGWx04HVgX5A"
+}
+
+
+// import { getNowPlaying } from '../../src/functions/fetch'
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+// 	const response = await getNowPlaying()
+
+// 	let spotify = {
+// 		artists: 'none',
+// 		album: 'none',
+// 		name: 'none',
+// 		playing: false,
+// 		url: 'none',
+// 	}
+
+// 	if (!(response.status === 204 || response.status > 400)) {
+// 		const { item: song, is_playing: playing, ...other } = await response.json()
+
+// 		if (playing && !(other.currently_playing_type === 'ad')) {
+// 			const { album, artists: sptfyArtists, external_urls, name } = song
+
+// 			const artists = sptfyArtists.map((artist) => artist.name).join(' + ')
+
+// 			console.log(album)
+
+// 			spotify = {
+// 				artists,
+// 				album: album.name,
+// 				name,
+// 				url: external_urls.spotify,
+// 				playing: true,
+// 			}
+// 		}
+// 	}
+
+// 	res.json(spotify)
+// }
