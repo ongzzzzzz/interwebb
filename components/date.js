cae834acc827aebf4d47063fd458bd0561973bf3
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2020-11-26 19:05:06 +0800

    date again

--- components/date.js
+++ components/date.js
@@ -1,7 +1,7 @@
 import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
-  // const date = parseISO(dateString)
-  // return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
-  return <time>{format(dateString, 'LLLL d, yyyy')}</time>
+  dateString ? dateString : '2020-20-20'
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
