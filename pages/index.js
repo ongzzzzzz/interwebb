28fc141ebaefedd4ddef863647332d40cc8abca1
Ong Zhi Zheng <Fogeinator@users.noreply.github.com>
2021-01-01 15:59:53 +0800

    add hackclub webring

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,22 @@ import styles from '../styles/Home.module.css'
 
 import { getSortedPostsData } from '../lib/posts'
 
+import { useEffect } from 'react'
 
 export default function Home({ allPostsData }) {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+  
+    script.src = "https://webring.hackclub.com/public/embed.min.js";
+    script.async = true;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
   
   return (
     
@@ -38,6 +52,13 @@ export default function Home({ allPostsData }) {
         <h2>this site is under construction 👀</h2>
         <h4>it's scaffolded from the starter nextjs app - i'm  still learning it!</h4>
       </section>
+
+      <div id="webring-wrapper" className={styles.webringcontainer}>
+        <a href="https://webring.hackclub.com/" id="previousBtn" className="webring-anchor" title="Previous">‹</a>
+        <a href="https://webring.hackclub.com/" className="webring-logo" title="Hack Club Webring" alt="Hack Club Webring"></a>
+        <a href="https://webring.hackclub.com/" id="nextBtn" className="webring-anchor" title="Next">›</a>
+        {/* <script src="https://webring.hackclub.com/public/embed.min.js"></script> */}
+      </div>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
